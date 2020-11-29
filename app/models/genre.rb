a30dd75d25e78f72f1dd4633c61985dfd219bf90
yada-hidetaka <70576263+yada-hidetaka@users.noreply.github.com>
2020-11-29 22:30:42 +0900

    内容変更

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,14 +1,14 @@
 class Genre < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-   { id: 2, name: '経済' },
-   { id: 3, name: '政治' },
-   { id: 4, name: '地域' },
-   { id: 5, name: '国際' },
-   { id: 6, name: 'IT' },
-   { id: 7, name: 'エンタメ' },
-   { id: 8, name: 'スポーツ' },
-   { id: 9, name: 'グルメ' },
+   { id: 2, name: 'Ruby' },
+   { id: 3, name: 'JavaScript' },
+   { id: 4, name: 'HTML' },
+   { id: 5, name: 'Python' },
+   { id: 6, name: 'IT系' },
+   { id: 7, name: 'Ruby on Rails' },
+   { id: 8, name: 'Java' },
+   { id: 9, name: 'React' },
    { id: 10, name: 'その他' }
   ]
 end
